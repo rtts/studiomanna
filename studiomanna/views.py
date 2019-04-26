6791ae0aa3d6ab0644db7333955dbcd4cd7c1454
Jaap Joris Vens <jj@rtts.eu>
2019-04-26 22:58:24 +0200

    Automated commit for deployment

--- studiomanna/views.py
+++ studiomanna/views.py
@@ -6,7 +6,7 @@ class PageWithEventsView(PageView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         today = datetime.datetime.today()
-        events = Event.objects.filter(date__gte=today)
+        events = Event.objects.filter(date__gte=today).order_by('type', 'date')
         context.update({
             'events': events,
         })
