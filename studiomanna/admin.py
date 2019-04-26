3d29e827e98008bdbe7a483273880f25eaa8af79
Jaap Joris Vens <jj@rtts.eu>
2019-04-26 21:58:00 +0200

    Automated commit for deployment

--- studiomanna/admin.py
+++ studiomanna/admin.py
@@ -3,5 +3,6 @@ from .models import Event
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
-    list_display = ['date', 'title', 'start_time', 'end_time']
+    list_display = ['date', 'type', 'title', 'start_time', 'end_time']
     list_display_links = ['title']
+    list_filter = ['type']
--- studiomanna/models.py
+++ studiomanna/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
+TYPES = [
+    (1, 'Yogales'),
+    (2, 'Shared dinner'),
+    (3, 'Workshop'),
+]
+
 class Event(models.Model):
     title = models.CharField('titel', max_length=32)
+    type = models.PositiveIntegerField('type', choices=TYPES)
     date = models.DateField('datum')
     start_time = models.TimeField('begintijd', blank=True, null=True)
     end_time = models.TimeField('eindtijd', blank=True, null=True)
